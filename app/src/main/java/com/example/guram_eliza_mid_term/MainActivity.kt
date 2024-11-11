91b6aac3ca350d803ea23cee500dc1a3fb21b266
Elizbarashvili Guram <elizbarashvili.guram18@gmail.com>
2024-11-11 14:12:00 +0400

    third_commit

--- app/src/main/java/com/example/guram_eliza_mid_term/MainActivity.kt
+++ app/src/main/java/com/example/guram_eliza_mid_term/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.text.InputFilter
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -22,6 +23,11 @@ findViewById<Button>(R.id.resetButton).setOnClickListener {
     if(!validateEditText(editText1, 3)) {
         editText1.error = "ntuu, "
     }
+    //++++++++++++++++++
+    val toast = Toast.makeText(this, "Custom Toast", Toast.LENGTH_LONG)
+//    toast.setGravity(Gravity.CENTER, 0, 0)
+
+    toast.show()
 }
         //=================
 //        editText1.backgroundTintList = ColorStateList.valueOf(Color.RED);
